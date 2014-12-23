15bc3f6dd1b7b02067d3ecb565615a9f0ce041eb
Gabe G'Sell <gabergg@gmail.com>
2014-12-23 12:39:22 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirdWave::Application.config.secret_key_base = '044a92b9d543e103117e7d5da40d4eb1116da2ad3d5bc2961ddf9f07f24f65bdd1a5cf669161223a2373b3b761f9597db89f00cdb8d240724831a17d92874600'
