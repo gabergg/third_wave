aeb2668ffbf16e4b8e2833131be7700b8e9cfdaa
Gabe G'Sell <gabergg@gmail.com>
2015-01-23 17:51:20 +1000

    working bean set, basic, needs rework

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,24 +1,41 @@
 class BeansController < ApplicationController
-  
+
   def index
+    @beans = Bean.all
   end
-  
+
   def create
+    @bean = Bean.new(bean_params)
+    if @bean.save
+      flash[:success] = "Review Submitted"
+      redirect_to beans_path
+    else
+      render 'new'
+    end
   end
-  
+
   def show
   end
-  
+
   def new
+    @bean = Bean.new
   end
-  
+
   def destroy
+    Bean.find(params[:id]).destroy
+    redirect_to beans_path
   end
-  
+
   def update
   end
-  
+
   def edit
   end
-  
+
+  private
+
+  def bean_params
+    params.require(:bean).permit(:name, :roaster, :origin, :location, :roast, :rating, :description, :review_date)
+  end
+
 end
